ca4746751adb4d4d8fdcb3161030cc7f78d9cf87
icdop <hclia@hotmail.com>
2021-02-26 16:23:50 +0800

    fix brokne link issue

--- tcl/STA_INDEX.tcl
+++ tcl/STA_INDEX.tcl
@@ -780,8 +780,8 @@ proc report_trend_group {{plot_dir ".trendchart"} {sta_group ""}} {
                   puts $fo "<td align=right> - </td>"
                 } else {
                   set STA_DQI($sta_dqi) $dqi_value
-#                  puts $fo "<td align=right><a href='$sta_run/$sta_group/$sta_mode/$sta_check.nvp_wns.png' target=sta_output> $dqi_value</a> </td>"
-                  puts $fo "<td align=right><a href='$sta_run/$sta_group/$sta_mode/$sta_check.nvp_wns.htm' target=sta_output> $dqi_value</a> </td>"
+#                  puts $fo "<td align=right><a href='../../$sta_run/$sta_group/$sta_mode/$sta_check.nvp_wns.png' target=sta_output> $dqi_value</a> </td>"
+                  puts $fo "<td align=right><a href='../../$sta_run/$sta_group/$sta_mode/$sta_check.nvp_wns.htm' target=sta_output> $dqi_value</a> </td>"
                 }
               }
               foreach sta_corner $STA_CORNER_LIST {
